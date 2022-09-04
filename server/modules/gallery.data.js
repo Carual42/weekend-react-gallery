61bb270f762d2ca0d235c97aa3b517d1d31d3912
Alex Carufel <Carual42@gmail.com>
2022-09-04 15:37:15 -0500

    images, titles, descriptions, added

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,9 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', title: 'The Goat', likes: 0 },
+    { id: 2, path: 'images/climb.jpg', description: 'Photo of rock climbing', title: 'Rock Climbing', likes: 0 },
+    { id: 3, path: 'images/controller.jpg', description: 'Neon controller vibes', title: 'My Escape', likes: 0 },
+    { id: 4, path: 'images/kayaking.jpg', description: 'Splish spalsh lets have blast', title: 'Rays and Waves', likes: 0 },
+
 ];
 
 module.exports = galleryItems;
